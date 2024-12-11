036166cafabf2cbf8dee9eeacefd01e700dd4e98
Quy <luongthanhquyv@gmail.com>
2024-12-11 22:20:50 +0700

    fix double bill and add time createdAt

--- src/routes.tsx
+++ src/routes.tsx
@@ -78,7 +78,7 @@ const AppRoutes: React.FC = () => {
                 <Route path="supplier" element={<SupplierPage />} />
             </Route>
             {/*employee*/}
-            <Route path="/employee" element={<ProtectedRoute role="manage" element={<EmployeeLayout />} />}>
+            <Route path="/employee" element={<ProtectedRoute role="user" element={<EmployeeLayout />} />}>
 
                 <Route path="bill" element={<BillList />} />
             </Route>
